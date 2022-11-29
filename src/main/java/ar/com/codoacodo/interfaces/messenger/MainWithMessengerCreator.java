200e104db0da78e769e93e2fe66d91289ff3d84a
hozlucas28 <hozlucas28@gmail.com>
2022-11-29 19:36:41 -0300

    Update MainWithMessengerCreator.java

--- src/main/java/ar/com/codoacodo/interfaces/messenger/MainWithMessengerCreator.java
+++ src/main/java/ar/com/codoacodo/interfaces/messenger/MainWithMessengerCreator.java
@@ -2,11 +2,9 @@ package ar.com.codoacodo.interfaces.messenger;
 
 public class MainWithMessengerCreator {
 	public static void main(String[] args) {
-		ISendMessage myInterface = MessengerCreator.createMessenger(target);
-		
 		String target = "B";
 		String message = "¡Hola Mundo!";
-		
+		ISendMessage myInterface = MessengerCreator.createMessenger(target);
 		
 		if (myInterface != null)
 			myInterface.action(message);
