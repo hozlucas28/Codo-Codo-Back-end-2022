9fb4d4859473c85d727633efa7fc2e920d07918a
hozlucas28 <hozlucas28@gmail.com>
2022-12-06 23:52:25 -0300

    Upload lesson

--- src/main/java/ar/com/codoacodo/controllers/CrudMain.java
+++ src/main/java/ar/com/codoacodo/controllers/CrudMain.java
@@ -1,3 +1,15 @@
+/* --------------------------------------------------------------------------
+ * APUNTES:
+ * 		   XXX.
+ *
+ *
+ * IMPORTANTE:
+ *  			  - Scanner(System.in) =  Me permite capturar las entradas
+ *										  del teclado.
+  *  			  - <VARIABLE>.nextInt() = Lee/Interpreta la variable como
+  *										   un entero.
+-------------------------------------------------------------------------- */
+
 package ar.com.codoacodo.controllers;
 
 import java.util.Scanner;
@@ -8,14 +20,44 @@ import ar.com.codoacodo.domain.Article;
 
 public class CrudMain {
 	public static void main(String[] args) throws Exception {
-		//INVESTIGAR ESCANER PARA LEER DATOS
-		Scanner teclado = new Scanner(System.in);
+		// Menú de navegación
+		System.out.println("• Por favor seleccioné una opción:");
+		System.out.println("[1] - Crear artículo");
+		System.out.println("[2] - Eliminar artículo");
+		System.out.println("[3] - Modificar artículo");
+		System.out.println("[4] - Buscar artículo por ID");
+		System.out.println("[5] - Salir");
+
+		Scanner keyword = new Scanner(System.in); // Comienza a capturar las entradas.
+		int optionSelected = -1;
 		
-		System.out.println("Ingrese una opcion 1, 2 o 3 etc..."); // TODO
-		
-		System.out.println("Ingrese el id del artículo");
-		Long id = teclado.nextLong();
-		System.out.println("Id es " + id);
-		teclado.close();
+		do {
+			optionSelected = keyword.nextInt(); // Guarda la interpretación de la entrada.
+			switch (optionSelected) {
+				case 1:
+					System.out.println("\n• Ingrese los datos del artículo...");
+					// TODO
+					break;
+				case 2:
+					System.out.println("\n• Ingrese el ID del artículo que quiere eliminar...");
+					// TODO
+					break;
+				case 3:
+					System.out.println("\n• Ingrese el ID del artículo que quiere modificar...");
+					// TODO
+					break;
+				case 4:
+					System.out.println("\n• Ingrese el ID del artículo que quiere buscar...");
+					// TODO
+					break;
+				case 5:
+					System.out.println("\n• Ingrese el ID del artículo que quiere buscar...");
+					// TODO
+					break;
+				default:
+					System.out.println("¡Error! La opción ingresada es invalida, vuelva a intentar...");
+					break;
+			}
+		} while (optionSelected < 1 && optionSelected > 5);
 	}
 }
--- src/main/java/ar/com/codoacodo/controllers/GetAllArticlesController.java
+++ src/main/java/ar/com/codoacodo/controllers/GetAllArticlesController.java
@@ -1,12 +1,34 @@
+/* --------------------------------------------------------------------------
+ * APUNTES:
+ * 		   La ruta para acceder al presente controlador seria aquella .
+ *
+ *
+ * IMPORTANTE:
+ *  			  - @WebServlet =  define la ruta para acceder a la página
+ *								   creada por la clase del paquete/controlador.
+-------------------------------------------------------------------------- */
+
 package ar.com.codoacodo.controllers;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 
 import ar.com.codoacodo.dao.IArticleDAO;
 import ar.com.codoacodo.dao.impl.ArticleDAOMySQLImpl;
 import ar.com.codoacodo.domain.Article;
 
-public class GetAllArticlesController {
+@WebServlet("/GetAllArticlesController") // Ruta de acceso --> http://localhost:8080/webapp/GetAllArticlesController
+public class GetAllArticlesController extends HttpServlet {
+	// Aplicación de consola
 	public static void main(String[] args) throws Exception {
 		IArticleDAO dao = new ArticleDAOMySQLImpl();
 		List<Article> articles = dao.getAllArticles();
@@ -14,5 +36,24 @@ public class GetAllArticlesController {
 		for (Article i : articles) {
 			System.out.println(i.getAttributes());
 		}
+	}
+
+	// Controlador - API ==> [Devuelve listado de artículos]
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		IArticleDAO dao = new ArticleDAOMySQLImpl();
+		List<Article> articles = new ArrayList<>();
+
+		try {
+			articles = dao.getAllArticles();
+			
+			for (Article i : articles) {
+				System.out.println(i.getAttributes());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		req.setAttribute("listado", articles);
+		getServletContext().getRequestDispatcher("/list.html").forward(req, resp);
 	}
 }
